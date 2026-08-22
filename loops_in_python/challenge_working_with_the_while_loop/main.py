e40a74861c341c4729d33627ccd6fb6252213b4f
marhab2006-bit <>
2026-08-22 11:52:52 +0000

    Challenge: Working with the While Loop

---
+++ loops_in_python/challenge_working_with_the_while_loop/main.py
@@ -0,0 +1,13 @@
+# Create variable
+i = 1
+squares = []
+
+# Write your code here
+while(i <= 10):
+    squares.append(i**2)
+    print(i, end=' ')
+    i = i + 1 
+
+
+# Testing
+print("The list of squares:", squares)
