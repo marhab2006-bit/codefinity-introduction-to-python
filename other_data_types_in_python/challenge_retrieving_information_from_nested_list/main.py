0ad5932ffa5cde3f1b976874ececf58920a3c5c4
marhab2006-bit <>
2026-08-22 10:37:51 +0000

    Challenge: Retrieving Information from Nested List

---
+++ other_data_types_in_python/challenge_retrieving_information_from_nested_list/main.py
@@ -0,0 +1,12 @@
+# Initial list
+people = [["Alex", 23], ["Noah", 34], ["Peter", 29], ["John", 41], ["Michelle", 35]]
+
+# Write your code here
+fourth_person = people[3]
+first_name = people[0][0]
+fifth_age = people[4][1]
+
+# Testing
+print("Full info about the fourth person:", fourth_person)
+print("The name of the first person:", first_name)
+print("The age of the fifth person:", fifth_age)
