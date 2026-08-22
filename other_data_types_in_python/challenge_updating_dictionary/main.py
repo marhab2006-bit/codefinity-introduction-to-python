ceb76475ab76a4fd157350c65ef3f0a7536a1457
marhab2006-bit <>
2026-08-22 11:27:33 +0000

    Challenge: Updating Dictionary

---
+++ other_data_types_in_python/challenge_updating_dictionary/main.py
@@ -0,0 +1,10 @@
+# Initial dictionary
+students_grades = {'Alex': 65, 'Noah': 89, 'Peter': 54}
+
+# Write your code here
+students_grades["John"] = 75
+students_grades["Alice"] = 92
+students_grades["Peter"] = 84
+
+# Testing
+print("Updated student's grades:", students_grades)
