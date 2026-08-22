803b7c7c6d31351671e1d358dc8f931fe991a2d6
marhab2006-bit <>
2026-08-22 10:49:33 +0000

    Challenge: Updating Tuple

---
+++ other_data_types_in_python/challenge_updating_tuple/main.py
@@ -0,0 +1,9 @@
+# Intial variables and values
+employees = ("Alex", "Noah", "Peter")
+interns = ("John", "Michelle")
+
+# Write your code here
+updated_employees = employees + interns
+
+# Testing
+print('Updated emoployees tuple:', updated_employees)
